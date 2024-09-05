d7920ce4024db1e4e9c87a39f358ea97eb20a587
Josue Igiraneza <igiranezaj28@gmail.com>
2024-09-05 18:02:42 +0200

    Nodejs Practical question 1

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const outputText =
+   "Hello this is the output in the output file, the POST request was made by postman";
+
+const server = http.createServer((req, res) => {
+   console.log(req.method);
+
+   res.setHeader("Content-Type", "text/plain");
+
+   if (req.method === "GET") {
+      fs.readFile("./input.txt", (err, data) => {
+         if (err) console.log(err.message);
+
+         res.statusCode = 200;
+         res.write(data);
+         res.end();
+      });
+   }
+
+   if (req.method === "POST") {
+      fs.writeFile("./output.txt", outputText, (err) => {
+         if (err) console.log(err.message);
+         else console.log("File created");
+      });
+
+      res.statusCode = 200;
+      res.end();
+   }
+});
+
+server.listen(3000, "localhost", () => {
+   console.log("Server started");
+});
